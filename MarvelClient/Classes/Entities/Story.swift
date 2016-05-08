75cc3c3805c0bcf3cc03702d29dda2e00981bc9a
Eduardo Arenas <eduardo.arenas@gamechanger.io>
2016-05-08 13:26:55 -0400

    Parse story entities

--- MarvelClient/Classes/Entities/Story.swift
+++ MarvelClient/Classes/Entities/Story.swift
@@ -12,15 +12,33 @@ import SwiftyJSON
 public struct Story: Entity {
   
   public let id: Int
-  public let modified: NSDate?
+  public let title: String
+  public let storyDescription: String
   public let resourceURI: String
+  public let type: String
+  public let modified: NSDate?
   public let thumbnail: Image
+  public let comics: ResourceList<ComicSummary>
+  public let stories: ResourceList<StorySummary>
+  public let events: ResourceList<EventSummary>
+  public let characters: ResourceList<CharacterSummary>
+  public let creators: ResourceList<CreatorSummary>
+  public let originalIssue: ComicSummary?
   
   public init(json: JSON) {
     self.id = json["id"].int!
-    self.modified = json["modified"].dateTime
+    self.title = json["title"].string!
+    self.storyDescription = json["description"].string!
     self.resourceURI = json["resourceURI"].string!
+    self.type = json["type"].string!
+    self.modified = json["modified"].dateTime
     self.thumbnail = Image(json: json["thumbnail"])
+    self.comics = ResourceList<ComicSummary>(json: json["comics"])
+    self.stories = ResourceList<StorySummary>(json: json["stories"])
+    self.events = ResourceList<EventSummary>(json: json["series"])
+    self.characters = ResourceList<CharacterSummary>(json: json["characters"])
+    self.creators = ResourceList<CreatorSummary>(json: json["creators"])
+    self.originalIssue = json["originalIssue"].type != .Null ? ComicSummary(json: json["originalIssue"]) : nil
   }
 }
 
